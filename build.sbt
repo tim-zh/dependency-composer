7d0cc21354bdc4d1837aa2817e069875195fc4a4
tim-zh <tim.zhlobich@gmail.com>
2016-09-28 12:47:21 +0300

    http4s

--- build.sbt
+++ build.sbt
@@ -10,5 +10,7 @@ resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
 
 libraryDependencies ++= Seq(
   "org.scalaj" %% "scalaj-http" % "2.3.0",
-  "com.typesafe.play" %% "play-json" % "2.3.8"
+  "com.typesafe.play" %% "play-json" % "2.3.8",
+  "org.http4s" %% "http4s-dsl" % "0.14.6",
+  "org.http4s" %% "http4s-blaze-server" % "0.14.6"
 )
--- src/main/scala/dcomposer/Main.scala
+++ src/main/scala/dcomposer/Main.scala
@@ -5,6 +5,7 @@ import scala.annotation.tailrec
 object Main extends App {
 
   println("Usage:")
+  println("  - type :web to open web ui")
   println("  - type an artifact name pattern")
   println("  - type :number (e.g. :1) to choose from search result")
   println("  - type :number again to choose version from search result, :0 is the latest release")
@@ -17,6 +18,8 @@ object Main extends App {
   def processInput(state: State): Unit = {
     io.Source.stdin.getLines().next() match {
       case ":x" =>
+      case ":web" =>
+        launchWebUi()
       case ":sbt" =>
         println(state.generateSbt)
         processInput(state)
@@ -31,6 +34,54 @@ object Main extends App {
         processInput(state.enterNumber(number))
       case pattern =>
         processInput(state.search(pattern))
+    }
+  }
+
+  def launchWebUi() = {
+    import org.http4s._
+    import org.http4s.dsl._
+    import org.http4s.server.blaze._
+    import org.http4s.server.syntax._
+    import scalaz.concurrent.Task
+
+    val helloWorldService = HttpService {
+      case GET -> Root / "hello" / name =>
+        Ok(s"Hello, $name.")
+    }
+    val helloWorldService2 = HttpService {
+      case GET -> Root / "hello2" / name =>
+        StaticFile.fromFile(new java.io.File("readme.md")).fold(NotFound())(Task.now)
+    }
+    val services = helloWorldService orElse helloWorldService2
+    val builder = BlazeBuilder.bindHttp(8080, "localhost").mountService(helloWorldService, "/").mountService(helloWorldService2, "/2")
+    val server = builder.run
+
+    java.awt.Desktop.getDesktop.browse(new java.net.URI("http://localhost:8080/"))
+    Thread.sleep(10000)
+  }
+
+  {
+    import java.io.IOException
+    import java.io.OutputStream
+    import java.net.InetSocketAddress
+    import com.sun.net.httpserver.HttpExchange
+    import com.sun.net.httpserver.HttpHandler
+    import com.sun.net.httpserver.HttpServer
+
+    val server = HttpServer.create(new InetSocketAddress(8080), 0)
+    server.createContext("/test", new Test.MyHandler)
+    server.setExecutor(null) // creates a default executor
+    server.start()
+
+    private[dcomposer] class MyHandler extends HttpHandler {
+      @throws[IOException]
+      def handle(t: HttpExchange) {
+        val response: String = "This is the response"
+        t.sendResponseHeaders(200, response.length)
+        val os: OutputStream = t.getResponseBody
+        os.write(response.getBytes)
+        os.close()
+      }
     }
   }
 
